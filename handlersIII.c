d7521f7522f988f59c43c8246ac129e7f5f6d741
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-18 11:39:06 +0300

    Implement mul handler

--- handlersIII.c
+++ handlersIII.c
@@ -65,8 +65,21 @@ void divHandler(stack_t **stack, unsigned int line_number)
  */
 void mulHandler(stack_t **stack, unsigned int line_number)
 {
-	stack = stack;
-	line_number = line_number;
+	unsigned int length = 0;
+	stack_t *p = *stack;
+
+	for (; p != NULL; p = p->next)
+		length++;
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free_entire_arr(argv);
+		if (*stack != NULL)
+			free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n *= (*stack)->n;
+	delete_node_at_index(stack, 0);
 }
 
 
