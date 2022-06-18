ff9bc0460850dd79b9ddbe71df28af4058d3dace
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-18 11:35:29 +0300

    Implement div handler

--- handlersIII.c
+++ handlersIII.c
@@ -35,8 +35,24 @@ void subHandler(stack_t **stack, unsigned int line_number)
  */
 void divHandler(stack_t **stack, unsigned int line_number)
 {
-	stack = stack;
-	line_number = line_number;
+	unsigned int length = 0;
+	stack_t *p = *stack;
+
+	for (; p != NULL; p = p->next)
+		length++;
+	if (length < 2 || (*stack)->n == 0)
+	{
+		if (length < 2)
+			fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		else
+			fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_entire_arr(argv);
+		if (*stack != NULL)
+			free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n /= (*stack)->n;
+	delete_node_at_index(stack, 0);
 }
 
 
